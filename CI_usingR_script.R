7eb5b3637fa73142f7c51f34b4401f0f0073b723
ellivromvvn <el_livro3485@yahoo.com>
2018-11-20 15:26:06 +0800

    writing, done with CI_usingR_script

--- CI_usingR_script.R
+++ CI_usingR_script.R
@@ -88,6 +88,7 @@ CI.pop.mean.ls <- function(x, c){
   CI <- c(xbar - E, xbar + E) #confidence interval
   return(round(CI, digits = 2)) #print confidence interval
 }
+
 # entering n
 CI.pop.mean.ls <- function(x, c, n){
   xbar <- mean(x)
@@ -98,30 +99,92 @@ CI.pop.mean.ls <- function(x, c, n){
   return(round(CI, digits = 2)) #print confidence interval
 }
 
-CI.pop.mean.ls(x=x,c = 0.95, n=32)
+
 
 #testing
 #storing the data
 x<-c(183, 183, 197, 209, 209, 212, 212, 212, 213, 213, 213,
      213, 213, 213, 213, 216, 216, 222, 222, 222, 222, 223,
      226, 226, 228, 228, 228, 228, 228, 229, 238, 238)
+
 CI.pop.mean.ls(x, 0.95)
+
+CI.pop.mean.ls(x=x,c = 0.95, n=32)
+
 #Example 2.
 
 
 #Confidence Intervals for the Mean (Small Samples)
 #Constructing a Confidence Interval for the Mean:t-Distribution
 
-
+CI.pop.mean.ss <- function(x, c){
+  n <- length(x)
+  df <- n-1
+  xbar <- mean(x)
+  s <- sd(x)
+  tsubc <- qt((1 + c)/2, df=df) #t scores
+  E <- tsubc * s/sqrt(n) #margin of error
+  CI <- c(xbar - E, xbar + E) #confidence interval
+  return(round(CI, digits = 2)) #print confidence interval
+}
+CI.pop.mean.ss(x=x, c=0.95)
 
 #Confidence Intervals for Population Proportions
 #Constructing a Confidence Interval for a Population Proportion
+CI.pop.prop <-function(x, n, c){
+  phat <- x/n
+  qhat <- 1-phat
+  a <- n*phat
+  b <- n*qhat
+  zsubc <- qnorm((1+c)/2)
+  E <- zsubc*sqrt(phat*qhat/n)
+  CI <- c(phat-E, phat+E)
+  ifelse(a>=5 & b>=5,
+    return(round(CI, digits = 2)),
+    return("samp dist of phat cannot be approx normal"))
+}
+CI.pop.prop(x=4431,n=7000,c=0.95)
 
 
 #Finding a Minimum Sample Size to Estimate p
 
+min.samp.size.pp <- function(E, c, p = 0.5){
+  qhat <- 1-p
+  zsubc <- qnorm((1+c)/2)
+  n <- p*qhat*(zsubc/E)^2
+  return(ceiling(n))
+}
+
+min.samp.size.pp(E=0.02,c=0.99)
 
 #Confidence Intervals for Variance and Standard Deviation 
 #Constructing a Confidence Interval for a Variance and Standard Deviation
 
+CI.var.sd <- function(x, c){
+  n <- length(x)
+  s.squared <- var(x)
+  ltcv <- qchisq((1-c)/2, df=n-1)
+  rtcv <- qchisq((1+c)/2, df=n-1)
+  lep <- ((n-1)*s.squared)/rtcv
+  rep <- ((n-1)*s.squared)/ltcv
+  CI.var <- c(lep,rep)
+  CI.sd <- sqrt(CI.var)
+  return(round(c(CI.var, CI.sd), digits = 2))
+}
 
+#additional options
+CI.var.sd <- function(x, c, CI = "both"){
+  n <- length(x)
+  s.squared <- var(x)
+  ltcv <- qchisq((1-c)/2, df=n-1)
+  rtcv <- qchisq((1+c)/2, df=n-1)
+  lep <- ((n-1)*s.squared)/rtcv
+  rep <- ((n-1)*s.squared)/ltcv
+  CI.var <- c(lep,rep)
+  CI.sd <- sqrt(CI.var)
+  ifelse(CI == "sd",
+         return(round(CI.sd, digits = 2)),
+         ifelse(CI == "var",
+                return(round(CI.var, digits = 2)),
+                return(round(c(CI.var, CI.sd), digits = 2))))
+}
