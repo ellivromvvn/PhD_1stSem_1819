952673414a31c141ee636599348354a1d7155e38
ellivromvvn <el_livro3485@yahoo.com>
2018-12-06 16:02:23 +0800

    added TRI and components' scores

--- data_prep.R
+++ data_prep.R
@@ -1,5 +1,9 @@
+##adding TRIScore and components' scores
+data$TRIScore<-rowMeans(data[,5:14])
+data$OPP<-rowMeans(data[,5:6])
+data$INN<-rowMeans(data[,7:9])
+data$DIS<-rowMeans(data[,15:16])
+data$INS<-rowMeans(data[,17:19])
+
 #data to tibble
 data <- as.tibble(data)
-
-# Creating mean column
-sum()
